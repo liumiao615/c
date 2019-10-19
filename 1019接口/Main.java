1c4c2ecfb3af56600833c82e9c2d871e29f3eacb
liumiao <1176411539@qq.com>
2019-10-19 23:18:09 +0800

    1

---
+++ 1019接口/Main.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random(20190905);
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+        System.out.println(random.nextInt(69));
+    }
+}
