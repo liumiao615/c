a0870a1f8b5addf03213dee01b0b769d8d693700
liumiao <1176411539@qq.com>
2019-10-27 16:30:20 +0800

    MySQL增查

---
+++ 1027数据库/1026.sql
@@ -0,0 +1,49 @@
+-- insert into tablename2(id) values(3);
+-- insert into tablename1 values(2,'世勋',24);
+-- insert into student(id,name,chinese,matn,english)values
+-- (1,'唐三藏',67,98,56),
+-- (2,'孙悟空',87.5,78,77);
+-- INSERT INTO student(id,NAME,chinese,matn,english)VALUES
+-- (3,'猪悟能',88,98.5,90),
+-- (4,'曹孟德',82,84,67),
+-- (5,'刘玄德',55.5,85,45),
+-- (6,'孙权',70,73,78.5),
+-- (7,'宋公明',75,65,30);
+-- select * from student;
+-- insert into student (id,name,chinese,matn,english)values
+-- (9,'老刘',100,80,90);
+-- (8,'吴世勋',98,98,98);
+-- SELECT * FROM student;
+-- select id,name,english from student;
+-- select id,name,10 from student;
+-- select id,name,english + 10 from student;
+-- SELECT id,NAME,chinese + matn +english 总分 FROM student;
+-- select matn from student;
+-- select distinct matn from student;
+-- SELECT * FROM student;
+-- select name,chinese from student order by chinese desc;
+-- select name,chinese + chinese + english + matn from student order by chinese + chinese + english + matn desc;
+-- select name,chinese + english + matn total from student order by total desc;
+-- select name,chinese ,english ,matn from student order by matn desc,english,chinese desc;
+
+-- select version();-- 设置默认时间
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
