3102d67efbff66a03a03280a14bc83f0c9726a71
liumiao <1176411539@qq.com>
2019-12-22 22:39:04 +0800

    Filepritace

---
+++ 20191222/Main.java
@@ -0,0 +1,83 @@
+package about_files;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    private static void listDir(File dir) {
+        File[] files = dir.listFiles();
+        for(File f:files){
+            if(f.isDirectory()){
+                listDir(f);
+            }else {
+                System.out.println(f.getAbsoluteFile() + ":" + f.length());
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        listDir(new File("D:\\报告类"));
+        //File file = new File("D:\\报告类");
+//        if(file.exists() && file.isDirectory()){
+//            File[] files = file.listFiles();
+//            for(File F:files){
+//                System.out.println(F);
+//            }
+//        }
+        //ListAllFile(file);
+//        File[] files = File.listRoots();
+//        for(File f:files){
+//            System.out.println(f);
+//            System.out.println(f.exists());
+//        }
+//        File file= File.createTempFile("iodem","end");
+//        System.out.println(file.getAbsoluteFile());
+        
+//        String Parent = "D:\\程序\\java idea\\2019-12-21IO\\测试目录";
+//        File file = new File(Parent);
+//        String destPath = "D:\\程序\\java idea\\2019-12-21IO\\新目录";
+//        File dest = new File(destPath);
+//        file.renameTo(dest);
+
+//        file.mkdirs();
+//        file.deleteOnExit();
+//        file.createNewFile();
+//        file.delete();
+//        file.deleteOnExit();
+//        String Path = "D:\\程序\\java idea\\2019-12-21IO";
+//        //String Parent = "D:\\程序\\java idea\\2019-12-21IO\\测试目录";
+//        File file = new File(Path);
+//        File[] files = file.listFiles();//将文件下的所有文件打印出来
+//        for(File f:files){
+//            System.out.println(f);
+//        }
+
+//        File file = new File(Parent);
+//        System.out.println(file.isHidden());//是否是隐藏文件
+//        file.length();
+
+        //File file = new File("D:\\程序\\java idea\\2019-12-21IO\\测试目录");
+//        System.out.println(file.exists());
+//        System.out.println(file.isDirectory());
+//        System.out.println(file.getFreeSpace());
+//        System.out.println(file.getUsableSpace());
+//        System.out.println(file.getTotalSpace());
+//        System.out.println(file.getAbsoluteFile());
+//        System.out.println(file.getParent());
+//        System.out.println(file.canRead());
+//        System.out.println(file.canWrite());
+//        System.out.println(file.canExecute());
+    }
+
+//    private static void ListAllFile(File file) {
+//        if(file.isDirectory()){
+//            File[] r = file.listFiles();
+//            if(r != null){
+//                for(File F:r){
+//                    ListAllFile(F);
+//                }
+//            }
+//        }else {
+//            System.out.println(file);
+//        }
+//    }
+}
