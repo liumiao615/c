24453bf7412126efc272ab869c54a6e74df29110
liumiao <1176411539@qq.com>
2020-02-21 21:43:51 +0800

    输出两数之和等于一个固定数的值

---
+++ 20200221/twosum.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class twosum {
+        public static int[] twoSum(int[] nums, int target){
+            int[] arr = new int[2];
+            int A = 0;
+            for(int i = 0;i < nums.length ;i++){
+                for(int j = i+1;j < nums.length;j++){
+                    if(nums[i] + nums[j] == target){
+                        arr[A++] = i;
+                        arr[A++] = j;
+                    }
+                }
+            }
+            return arr;
+        }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+         System.out.println(Arrays.toString(twoSum(nums,27)));
+    }
+}
+
