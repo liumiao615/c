282b1440798e73755615becdf5539efbf0775f11
liumiao <1176411539@qq.com>
2020-03-14 22:38:45 +0800

    百万富翁问题

---
+++ 20200314/Money.java
@@ -0,0 +1,14 @@
+public class Money {
+    public static void main(String[] args) {
+        long Rich = 0;
+        long Stranger = 0;
+
+        for(int i = 1;i <= 30;i++){
+            Rich += 10;
+            Stranger += Math.pow(2,i - 1);
+
+        }
+        System.out.println(Rich +  " " + Stranger);
+
+    }
+}
