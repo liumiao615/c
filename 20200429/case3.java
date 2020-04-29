79af70621826daea92f574fa959acace00a053ae
liumiao <1176411539@qq.com>
2020-04-29 22:57:32 +0800

    线程demo2

---
+++ 20200429/case3.java
@@ -0,0 +1,41 @@
+package why;
+
+public class case3 {
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("我在 method2 中打印");
+                // 进程会暂停运行 1 秒
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void method2() {
+        Thread thread = new MyThread();
+        thread.start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // 两个死循环可以同时开始执行，没有一个卡住另一个的情况
+        method2();
+        while (true) {
+            System.out.println("我在 main 中打印");
+            // 进程会暂停运行 1 秒
+            Thread.sleep(1000);
+        }
+    }
+
+    private static void method1() throws InterruptedException {
+        while (true) {
+            System.out.println("我在 method1 中打印");
+            // 进程会暂停运行 1 秒
+            Thread.sleep(1000);
+        }
+    }
+}
