93df6cbd3c9f3dc93d1536504a3fb45b5eadefbd
liumiao <1176411539@qq.com>
2020-06-16 22:56:46 +0800

    通过Class实例获取字段信息

---
+++ 20200616/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Class stdClass = Student.class;
+        // 获取public字段"score":
+        System.out.println(stdClass.getField("score"));
+        // 获取继承的public字段"name":
+        System.out.println(stdClass.getField("name"));
+        // 获取private字段"grade":
+        System.out.println(stdClass.getDeclaredField("grade"));
+    }
+}
+
+class Student extends Person {
+    public int score;
+    private int grade;
+}
+
+class Person {
+    public String name;
+}
+
+
+//public int Student.score
+//public java.lang.String Person.name
+//private int Student.grade
