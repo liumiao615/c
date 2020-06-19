3b9e61dfb090698f3f1186295810fa9a84f7bf3b
liumiao <1176411539@qq.com>
2020-06-19 23:35:21 +0800

    用迭代器方法遍历list

---
+++ 20200619/Main.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+//用迭代器的方法遍历list
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = List.of("apple", "pear", "banana");
+			for(Iterator<String> it = list.iterator();it.hasNext()){
+				String s = it.next();
+				System.out.println(s);
+			}
+        }
+    }
+}
