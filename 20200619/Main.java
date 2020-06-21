18cad6275e7093ce78feb8d4aeb21a7a03d3036d
liumiao <1176411539@qq.com>
2020-06-21 23:13:33 +0800

    /

--- 20200619/Main.java
+++ 20200619/Main.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-//用迭代器的方法遍历list
+////用迭代器的方法遍历list
 public class Main {
     public static void main(String[] args) {
         List<String> list = List.of("apple", "pear", "banana");
