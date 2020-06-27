63a41fece2f9435ebfd92c2abd79c7dee3d7e9e1
liumiao <1176411539@qq.com>
2020-06-27 23:13:53 +0800

    双端队列demo

---
+++ 20200627/Main.java
@@ -0,0 +1,15 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main{
+	public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<>();
+        deque.offerLast("A"); // A
+        deque.offerLast("B"); // B -> A
+        deque.offerFirst("C"); // B -> A -> C
+        System.out.println(deque.pollFirst()); // C, 剩下B -> A
+        System.out.println(deque.pollLast()); // B
+        System.out.println(deque.pollFirst()); // A
+        System.out.println(deque.pollFirst()); // null
+    }
+}
