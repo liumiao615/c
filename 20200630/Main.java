69789e974084ebc586e5af071155aef071ddb5ec
liumiao <1176411539@qq.com>
2020-06-30 22:36:46 +0800

    Collections的排序，洗牌方法

---
+++ 20200630/Main.java
@@ -0,0 +1,40 @@
+//Collections是JDK提供的工具类，同样位于java.util包中。
+//它提供了一系列静态方法，能更方便地操作各种集合。
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("apple");
+        list.add("pear");
+        list.add("orange");
+        // 排序前:
+        System.out.println(list);
+        Collections.sort(list);
+        // 排序后:
+        System.out.println(list);
+    }
+}
+
+//洗牌算法
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i=0; i<10; i++) {
+            list.add(i);
+        }
+        // 洗牌前:
+        System.out.println(list);
+        Collections.shuffle(list);
+        // 洗牌后:
+        System.out.println(list);
+    }
+}
+
+
+
+
+
+
+
+
+
