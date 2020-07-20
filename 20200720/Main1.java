7439d0999b3e03dd7385f227be343c2167d458e6
liumiao <1176411539@qq.com>
2020-07-20 23:07:09 +0800

    数组查找

---
+++ 20200720/Main1.java
@@ -0,0 +1,19 @@
+public class Solution{
+	public boolean Find(int target,int[][]array){
+			if(array == null){
+				return false;
+			}
+			int i = 0;
+			int j = array[0].length - 1;
+			while(i < array.length && j>= 0){
+				if(target < array[i][j]){
+					j--;
+				}else if(target > array[i][j]){
+					i++;
+				}else{
+					return true;
+			}
+		}
+		return false;
+	}
+}
